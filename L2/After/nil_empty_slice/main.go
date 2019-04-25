9d5219a67ec018112d60fc4109c7db4f04658ca3
zzf1208 <zhangzhifan1208@163.com>
2019-04-25 16:36:33 +0800

    nil切片例子补充

--- L2/After/nil_empty_slice/main.go
+++ L2/After/nil_empty_slice/main.go
@@ -5,19 +5,25 @@ import "fmt"
 func main() {
 	//空切片
 	b := []int{}
-	fmt.Printf("b为:%v\n", b)
+	fmt.Printf("b为:%#v\n", b) //%#v表示以go语言语法形式显示值
 	fmt.Printf("b的内存地址：%p\n", b)
+	b = append(b, 2)
+	fmt.Printf("After:b为:%#v\n", b)
+	fmt.Printf("After:b的内存地址：%p\n", b)
 
 	c := []string{}
-	fmt.Printf("c为:%v\n", c)
+	fmt.Printf("c为:%#v\n", c)
 	fmt.Printf("c的内存地址：%p\n", c)
+	c = append(c, "MVP")
+	fmt.Printf("After:c为:%#v\n", c)
+	fmt.Printf("After:c的内存地址：%p\n", c)
 
 	//nil切片
 	var d []int
-	fmt.Printf("d为:%v\n", d)
+	fmt.Printf("d为:%#v\n", d)
 	fmt.Printf("d的内存地址：%p\n", d)
 	d = append(d, 1)
-	fmt.Printf("After:d为:%v\n", d)
+	fmt.Printf("After:d为:%#v\n", d)
 	fmt.Printf("After:d的内存地址：%p\n", d)
 
 }
--- L2/After/nil_empty_slice2/main.go
+++ L2/After/nil_empty_slice2/main.go
@@ -4,15 +4,21 @@ import "fmt"
 
 func main() {
 	// //空切片
-	// b := []int{}
+	// b := []int{1}
 	// b[0] = 1
 	// fmt.Printf("b为:%v\n", b)
 	// fmt.Printf("b的内存地址：%p\n", b)
 
-	//nil切片
-	var d []int
-	d[0] = 1
-	fmt.Printf("d为:%v\n", d)
-	fmt.Printf("d的内存地址：%p\n", d)
+	// // //nil切片
+	// // var d []int
+	// // d[0] = 1
+	// // fmt.Printf("d为:%v\n", d)
+	// // fmt.Printf("d的内存地址：%p\n", d)
 
+	type Human struct {
+		Name string
+	}
+
+	var people = Human{Name: "zhangsan"}
+	fmt.Printf("%+v", people)
 }
